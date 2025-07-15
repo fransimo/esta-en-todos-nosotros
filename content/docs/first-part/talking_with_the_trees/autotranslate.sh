5b73955c95d60c67e01570215bb2e07f8bfa4ee3
Fran Simó <fransimo@gmail.com>
2025-07-15 22:05:45 +0300

    autotranslate.sh

--- content/docs/first-part/talking_with_the_trees/autotranslate.sh
+++ content/docs/first-part/talking_with_the_trees/autotranslate.sh
@@ -13,3 +13,36 @@ pandoc tmp.md  -f gfm -t pdf -o tmp.pdf  \
       -V mainfont="DejaVu Serif" \
       -V monofont="DejaVu Sans Mono" \
       -V fontsize=12pt ## --toc
+
+
+curl -X 'POST' \
+  'http://localhost:5555/translate_file' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: multipart/form-data' \
+  -F 'file=@tmp.docx;type=application/vnd.openxmlformats-officedocument.wordprocessingml.document' \
+  -F 'source=es' \
+  -F 'target=en' \
+  -F 'api_key=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+
+{
+  "translatedFileUrl": "http://localhost:5555/download_file/0537aa1f-2271-4026-8c25-c71f4c5a3056.tmp_en.docx"
+}
+
+
+
+
+# export API_KEY=...
+#
+# curl -X POST 'https://api-free.deepl.com/v2/translate' \
+  #--header 'Authorization: DeepL-Auth-Key [yourAuthKey]' \
+  #--header 'Content-Type: application/json' \
+  #--data '{
+  #    "text": [
+  #        "Hello, world!"
+  #    ],
+  #    "target_lang": "DE"
+  #}'
+# curl -X POST 'https://api-free.deepl.com/v2/translate' \
+#  --header 'Authorization: DeepL-Auth-Key $API_KEY' \
+#  --form 'target_lang=EN' \
+#  --form 'file=@tmp.docx'

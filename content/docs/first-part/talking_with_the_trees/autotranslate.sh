6b20c16759b43fae0be8b0af52c69992367012cb
Fran Simó <fransimo@gmail.com>
2025-07-15 18:37:58 +0300

    Talking with trees new version

--- content/docs/first-part/talking_with_the_trees/autotranslate.sh
+++ content/docs/first-part/talking_with_the_trees/autotranslate.sh
@@ -7,4 +7,9 @@ sed '1{/^---$/!q;};1,/^---$/d' _index.es.md > tmp.md
 
 pandoc tmp.md -t docx -o tmp.docx
 
-pandoc tmp.md  -f gfm -t pdf -o tmp.pdf  -V geometry:"portrait, margin=2cm, a4paper" -V mainfont="DejaVu Serif" -V monofont="DejaVu Sans Mono"  ## --toc
+pandoc tmp.md  -f gfm -t pdf -o tmp.pdf  \
+      --include-in-header pdf_prop.tex \
+      -V geometry:"portrait, margin=2cm, a4paper" \
+      -V mainfont="DejaVu Serif" \
+      -V monofont="DejaVu Sans Mono" \
+      -V fontsize=12pt ## --toc
